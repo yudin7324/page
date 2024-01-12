b80f56c12e74d130f2e55617372f43dd9fb9270e
Ilya Yudin <yudin2402@gmail.com>
2024-01-12 10:20:09 +0500

    initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header"
+
+function App() {
+
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="main__wrapper">
+          <Header />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './_header.scss'
+
+const Loader: FC = () => {
+  return (
+    <header className='header'>
+      header
+    </header>
+  )
+}
+
+export default Loader
